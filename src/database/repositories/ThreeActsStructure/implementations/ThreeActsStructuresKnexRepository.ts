8e16b80554b33b0db3cd0ae873bf9f15c98e243a
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-10-25 16:33:23 -0300

    Refactor three acts structure repository

--- src/database/repositories/ThreeActsStructure/implementations/ThreeActsStructuresKnexRepository.ts
+++ src/database/repositories/ThreeActsStructure/implementations/ThreeActsStructuresKnexRepository.ts
@@ -1,5 +1,4 @@
 import { ThreeActsStructure } from '@modules/ThreeActsStructures/models/ThreeActsStructure';
-import { Either, left, right } from '@shared/core/error/Either';
 import { db } from '@database/index';
 import { ThreeActsStructuresRepository } from '../contracts/ThreeActsStructuresRepository';
 import { ThreeActsStructuresKnexMapper } from './ThreeActsStructuresKnexMapper';
@@ -7,43 +6,25 @@ import { ThreeActsStructuresKnexMapper } from './ThreeActsStructuresKnexMapper';
 export class ThreeActsStructuresKnexRepository
   implements ThreeActsStructuresRepository
 {
-  async create(
-    threeActsStructure: ThreeActsStructure
-  ): Promise<Either<{}, {}>> {
-    try {
-      await db('three_acts_structures').insert(
-        ThreeActsStructuresKnexMapper.toKnex(threeActsStructure)
-      );
-      return right({});
-    } catch (err) {
-      console.log(err);
-      return left({});
-    }
+  async create(threeActsStructure: ThreeActsStructure): Promise<void> {
+    await db('three_acts_structures').insert(
+      ThreeActsStructuresKnexMapper.toKnex(threeActsStructure)
+    );
   }
 
-  async save(threeActsStructure: ThreeActsStructure): Promise<Either<{}, {}>> {
-    try {
-      await db('three_acts_structures')
-        .where({ id: threeActsStructure.id.toString() })
-        .update(ThreeActsStructuresKnexMapper.toKnex(threeActsStructure));
-      return right({});
-    } catch (err) {
-      console.log(err);
-      return left({});
-    }
+  async save(threeActsStructure: ThreeActsStructure): Promise<void> {
+    await db('three_acts_structures')
+      .where({ id: threeActsStructure.id.toString() })
+      .update(ThreeActsStructuresKnexMapper.toKnex(threeActsStructure));
   }
 
-  async findById(id: string): Promise<Either<{}, ThreeActsStructure | null>> {
-    try {
-      const threeActsStructure = await db('three_acts_structures')
-        .where({ id })
-        .first();
+  async findById(id: string): Promise<ThreeActsStructure | null> {
+    const threeActsStructure = await db('three_acts_structures')
+      .where({ id })
+      .first();
 
-      if (!threeActsStructure) return right(null);
-      return right(ThreeActsStructuresKnexMapper.toEntity(threeActsStructure));
-    } catch (err) {
-      console.log(err);
-      return left({});
-    }
+    if (!threeActsStructure) return null;
+
+    return ThreeActsStructuresKnexMapper.toEntity(threeActsStructure);
   }
 }
