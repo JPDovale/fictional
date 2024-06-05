ff28c03bc396f26a329ece32d9a7e87280f0cd9e
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-06-05 05:06:09 -0300

    feat: adjusts on interface

--- src/renderer/pages/projects/[id]/time-lines/[id]/index.tsx
+++ src/renderer/pages/projects/[id]/time-lines/[id]/index.tsx
@@ -8,7 +8,7 @@ export function ProjectTimelinePage() {
   const { useTimeline } = useProject({ projectId });
   const { timeline, useEvents } = useTimeline({ timelineId });
 
-  const { datesInCronologicalOrder, existentsYears, dates } = useEvents();
+  const { dates } = useEvents();
 
   if (!timeline) return <NotFound />;
 
@@ -20,12 +20,12 @@ export function ProjectTimelinePage() {
         <div className="flex flex-col max-w-1/3">
           {dates.map((date) => (
             <div
-              className="flex flex-col gap-2 border-gray200 border-l-2 pb-16"
+              className="flex flex-col gap-2 border-purple500 border-l-2 pb-16"
               key={date.yearName}
             >
               <div className="flex gap-2 items-center">
-                <div className="w-8 h-8 bg-gray100 rounded-full -ml-4"></div>
-                <span className="text-4xl font-bold opacity-60">
+                <div className="w-8 h-8 bg-gray400 rounded-full shadow-defaultDark -ml-4"></div>
+                <span className="text-3xl font-bold opacity-60 ml-2">
                   {date.yearName}
                 </span>
               </div>
@@ -36,8 +36,8 @@ export function ProjectTimelinePage() {
                   key={`${date.yearName}-${month.monthName}`}
                 >
                   <div className="flex gap-2 items-center">
-                    <div className="w-6 h-6 bg-gray100 rounded-full -ml-3"></div>
-                    <span className="text-lg font-bold uppercase opacity-60">
+                    <div className="w-6 h-6 bg-gray400 rounded-full shadow-defaultDark -ml-3"></div>
+                    <span className="text-lg font-bold uppercase opacity-60 ml-3">
                       {month.monthName}
                     </span>
                   </div>
@@ -48,23 +48,23 @@ export function ProjectTimelinePage() {
                       key={`${date.yearName}-${month.monthName}-${day.day}`}
                     >
                       <div className="flex gap-2 items-center">
-                        <div className="w-3 h-3 bg-gray100 rounded-full -ml-1.5"></div>
-                        <span className="font-bold">{day.day}</span>
+                        <div className="w-3 h-3 bg-gray400 rounded-full shadow-defaultDark -ml-1.5"></div>
+                        <span className="font-bold ml-4">{day.day}</span>
                       </div>
 
                       {day.events.map((event) => (
                         <div className="flex flex-col gap-2" key={event.id}>
                           <div className="flex gap-2">
-                            <div className="w-2 h-2 bg-gray100 rounded-full -ml-1 mt-1"></div>
+                            <div className="w-2 h-2 bg-gray400 rounded-full shadow-defaultDark -ml-1 mt-1"></div>
                             <span className="text-xs opacity-60 font-bold">
                               Dia {event.dateObject.day} de {month.monthName} de{' '}
-                              {date.yearName} às
+                              {date.yearName.replaceAll(' Anos ', ' ')} às{' '}
                               {event.dateObject.hour}:{event.dateObject.minute}:
                               {event.dateObject.second}
                             </span>
                           </div>
 
-                          <p className="ml-3 text-sm text-justify w-full p-2 rounded-lg shadow-xl mb-6">
+                          <p className="ml-3 text-sm text-justify w-full p-2 rounded-lg shadow-defaultDark mb-6">
                             {event.event}
                           </p>
                         </div>
